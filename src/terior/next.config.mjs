33913635d639de43fcc76ec5188440529e45f3fe
Vlad <dog792804@gmail.com>
2024-02-21 00:21:56 +0200

    fix

--- src/terior/next.config.mjs
+++ src/terior/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
   output: 'export',
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
